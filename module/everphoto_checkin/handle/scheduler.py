92818f61fcc4a098b50f2a738ad3dccd8e5e55a0
muumlover <muumlover@live.com>
2019-10-15 23:11:14 +0800

    更新列表可签到判断条件

--- module/everphoto_checkin/handle/scheduler.py
+++ module/everphoto_checkin/handle/scheduler.py
@@ -10,17 +10,33 @@ async def auto_check_in():
 
     with data_manager('task_list', collection='everphoto_checkin') as task_list:
         for task in task_list.values():
+            # 上次签到时间为今天则跳过
             last_time = task.get('last_time', '')
             if last_time and int(time.time() / (60 * 60 * 24)) - int(last_time.timestamp() / (60 * 60 * 24)) == 0:
-                print(f'task {task["id"]} skip')
+                print(f'task {task["id"]} today already checkin')
                 continue
 
+            # 查询任务失败跳过
+            resp = await EverPhoto.checkin_query(task['token'])
+            if resp['code'] != 0:
+                print(f'task {task["id"]} query fail', resp)
+                continue
+
+            # 查询到今日已签到跳过
+            resp_data = resp.get('data', {})
+            task['can_check_in'] = resp_data['can_checkin']
+            if resp_data['can_checkin'] is False:
+                task['last_time'] = datetime.now()
+                print(f'task {task["id"]} query today already checkin')
+                continue
+
+            # 签到失败跳过
             resp = await EverPhoto.checkin_post(task['token'])
             if resp['code'] != 0:
-                print(f'task {task["id"]} fail', resp)
+                print(f'task {task["id"]} checkin fail', resp)
                 continue
 
-            print(f'task {task["id"]} success')
+            print(f'task {task["id"]} checkin success')
             resp_data = resp['data']
             if resp_data['checkin_result']:
                 task['last_time'] = datetime.now()
