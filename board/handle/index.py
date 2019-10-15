9d69ebc7dc24a6f5a6b8231fc38b70b7b12e2842
wangsen <wangsen@primeton.com>
2019-10-15 12:14:08 +0800

    增加模块引用

--- board/handle/index.py
+++ board/handle/index.py
@@ -1,24 +1,28 @@
 import aiohttp_jinja2
 from aiohttp.web_urldispatcher import View
 
-from module.everphoto_checkin import app
-
 
 class IndexView(View):
     @aiohttp_jinja2.template('index.jinja2')
     async def get(self):
+        sub_menu = []
+        for module in self.request.app['module_all'].values():
+            if not module['loaded']:
+                continue
+            lib = module['lib']
+            sub_menu.append({
+                'icon': '',
+                'title': lib.title,
+                'link': lib.app.router['ec_task_list'].url_for(),
+            })
+
         return {
             'name': 'AutoCheckIn',
             'main_menu': [
                 {
                     'icon': '',
                     'title': '签到中心',
-                    'sub_menu': [
-                        {
-                            'icon': '',
-                            'title': '时光相册',
-                            'link': app.router['ec_task_list'].url_for(),
-                        },
+                    'sub_menu': sub_menu + [
                         {
                             'icon': '',
                             'title': '敬请期待',
--- module/everphoto_checkin/__init__.py
+++ module/everphoto_checkin/__init__.py
@@ -1,6 +1,7 @@
 from .app import everphoto_checkin as app
 from .handle.scheduler import auto_check_in
 
+title = '时光相册'
 name = 'everphoto_checkin'
 jobs = [
     # {'func': test, 'trigger': 'cron', 'args': [app], 'hour': '23', 'minute': '0', 'id': 'expiry'}
